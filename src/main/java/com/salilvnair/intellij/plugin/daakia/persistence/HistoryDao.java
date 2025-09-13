73901665273cd5fef42d9a86ae4e20c996c4cf9d
Salil V Nair <salilvnairgithub@gmail.com>
2025-09-13 15:10:38 -0500

    Refactor history storage to structured SQL

--- src/main/java/com/salilvnair/intellij/plugin/daakia/persistence/HistoryDao.java
+++ src/main/java/com/salilvnair/intellij/plugin/daakia/persistence/HistoryDao.java
@@ -3,7 +3,6 @@ package com.salilvnair.intellij.plugin.daakia.persistence;
 import com.intellij.openapi.application.ApplicationManager;
 import com.salilvnair.intellij.plugin.daakia.ui.core.model.DaakiaBaseStoreData;
 import com.salilvnair.intellij.plugin.daakia.ui.core.model.DaakiaHistory;
-import com.salilvnair.intellij.plugin.daakia.ui.utils.JsonUtils;
 
 import java.sql.*;
 import java.util.*;
@@ -17,14 +16,26 @@ public class HistoryDao {
         List<DaakiaHistory> list = new ArrayList<>();
         try (Connection conn = DaakiaDatabase.getInstance().getHistoryConnection();
              Statement stmt = conn.createStatement()) {
-            ResultSet rs = stmt.executeQuery("SELECT id, data FROM history_records WHERE active='Y'");
+            ResultSet rs = stmt.executeQuery("SELECT id,display_name,request_type,url,headers,response_headers,request_body,response_body,pre_request_script,post_request_script,created_date,size_text,time_taken,status_code,auth_info,uuid FROM history_records WHERE active='Y'");
             while (rs.next()) {
-                String json = rs.getString("data");
-                try {
-                    DaakiaHistory h = JsonUtils.jsonToPojo(json, DaakiaHistory.class);
-                    h.setId(rs.getInt("id"));
-                    list.add(h);
-                } catch (Exception ignore) {}
+                DaakiaHistory h = new DaakiaHistory();
+                h.setId(rs.getInt("id"));
+                h.setDisplayName(rs.getString("display_name"));
+                h.setRequestType(rs.getString("request_type"));
+                h.setUrl(rs.getString("url"));
+                h.setHeaders(rs.getString("headers"));
+                h.setResponseHeaders(rs.getString("response_headers"));
+                h.setRequestBody(rs.getString("request_body"));
+                h.setResponseBody(rs.getString("response_body"));
+                h.setPreRequestScript(rs.getString("pre_request_script"));
+                h.setPostRequestScript(rs.getString("post_request_script"));
+                h.setCreatedDate(rs.getString("created_date"));
+                h.setSizeText(rs.getString("size_text"));
+                h.setTimeTaken(rs.getString("time_taken"));
+                h.setStatusCode(rs.getInt("status_code"));
+                h.setAuthInfo(rs.getString("auth_info"));
+                h.setUuid(rs.getString("uuid"));
+                list.add(h);
             }
         } catch (SQLException ignore) {}
         return list.stream()
@@ -35,14 +46,26 @@ public class HistoryDao {
         List<DaakiaHistory> list = new ArrayList<>();
         try (Connection conn = DaakiaDatabase.getInstance().getHistoryConnection();
              Statement stmt = conn.createStatement()) {
-            ResultSet rs = stmt.executeQuery("SELECT id, data FROM history_records WHERE active='N'");
+            ResultSet rs = stmt.executeQuery("SELECT id,display_name,request_type,url,headers,response_headers,request_body,response_body,pre_request_script,post_request_script,created_date,size_text,time_taken,status_code,auth_info,uuid FROM history_records WHERE active='N'");
             while (rs.next()) {
-                String json = rs.getString("data");
-                try {
-                    DaakiaHistory h = JsonUtils.jsonToPojo(json, DaakiaHistory.class);
-                    h.setId(rs.getInt("id"));
-                    list.add(h);
-                } catch (Exception ignore) {}
+                DaakiaHistory h = new DaakiaHistory();
+                h.setId(rs.getInt("id"));
+                h.setDisplayName(rs.getString("display_name"));
+                h.setRequestType(rs.getString("request_type"));
+                h.setUrl(rs.getString("url"));
+                h.setHeaders(rs.getString("headers"));
+                h.setResponseHeaders(rs.getString("response_headers"));
+                h.setRequestBody(rs.getString("request_body"));
+                h.setResponseBody(rs.getString("response_body"));
+                h.setPreRequestScript(rs.getString("pre_request_script"));
+                h.setPostRequestScript(rs.getString("post_request_script"));
+                h.setCreatedDate(rs.getString("created_date"));
+                h.setSizeText(rs.getString("size_text"));
+                h.setTimeTaken(rs.getString("time_taken"));
+                h.setStatusCode(rs.getInt("status_code"));
+                h.setAuthInfo(rs.getString("auth_info"));
+                h.setUuid(rs.getString("uuid"));
+                list.add(h);
             }
         } catch (SQLException ignore) {}
         return list;
@@ -54,13 +77,26 @@ public class HistoryDao {
         try (Connection conn = DaakiaDatabase.getInstance().getHistoryConnection();
              Statement stmt = conn.createStatement()) {
             stmt.executeUpdate("DELETE FROM history_records WHERE active='Y'");
-            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO history_records(data,active) VALUES(?,?)")) {
+            String insert = "INSERT INTO history_records(display_name,request_type,url,headers,response_headers,request_body,response_body,pre_request_script,post_request_script,created_date,size_text,time_taken,status_code,auth_info,uuid,active) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            try (PreparedStatement ps = conn.prepareStatement(insert)) {
                 for (DaakiaHistory h : flat) {
-                    try {
-                        ps.setString(1, JsonUtils.pojoToJson(h));
-                        ps.setString(2, "Y");
-                        ps.executeUpdate();
-                    } catch (Exception ignore) {}
+                    ps.setString(1, h.getDisplayName());
+                    ps.setString(2, h.getRequestType());
+                    ps.setString(3, h.getUrl());
+                    ps.setString(4, h.getHeaders());
+                    ps.setString(5, h.getResponseHeaders());
+                    ps.setString(6, h.getRequestBody());
+                    ps.setString(7, h.getResponseBody());
+                    ps.setString(8, h.getPreRequestScript());
+                    ps.setString(9, h.getPostRequestScript());
+                    ps.setString(10, h.getCreatedDate());
+                    ps.setString(11, h.getSizeText());
+                    ps.setString(12, h.getTimeTaken());
+                    ps.setObject(13, h.getStatusCode());
+                    ps.setString(14, h.getAuthInfo());
+                    ps.setString(15, h.getUuid());
+                    ps.setString(16, "Y");
+                    ps.executeUpdate();
                 }
             }
         } catch (SQLException ignore) {}
